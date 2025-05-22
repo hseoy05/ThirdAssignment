0a8a3c5922076ab28c49809a6dc156fe4813100a
SeoYeon <tjdus121234@korea.ac.kr>
2025-05-22 10:15:49 +0900

    Update loginCheck.php

--- src/loginCheck.php
+++ src/loginCheck.php
@@ -1,32 +1,22 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LoginCheck</title>
-</head>
-<body>
-    <?php
-    $conn = new mysqli("db", "root", "root", "testdb");
-    if ($conn->connect_error) {die("DB connect Fail: " . $conn->connect_error);}
+<?php
+$conn = new mysqli("db", "root", "root", "testdb");
+if ($conn->connect_error) {die("DB connect Fail: " . $conn->connect_error);}
 
-    $userId = $_POST['userId'] ?? '';
-    $userPassword = $_POST['userPassword'] ?? '';
+$userId = $_POST['userId'] ?? '';
+$userPassword = $_POST['userPassword'] ?? '';
 
-    if($userId && $userPassword) {
-        $sql = "SELECT * FROM users WHERE userId = ? AND userPassword = ?";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("ss", $userId, $userPassword);
-        $stmt->execute();
-        $result = $stmt->get_result();
+if($userId && $userPassword) {
+    $sql = "SELECT * FROM users WHERE userId = ? AND userPassword = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $userId, $userPassword);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-        if($result->num_rows === 1) {
-            $row = $result->fetch_assoc();
-            $userName = $row['userName'];
-            echo "Login Success!\nHello, " . htmlspecialchars($userName)."!";
-        } else { echo "Failed to Login."; }
-    }
-    $conn -> close();
-    ?>
-</body>
-</html>
+    if($result->num_rows === 1) {
+        $row = $result->fetch_assoc();
+        $userName = $row['userName'];
+        echo "Login Success!\nHello, " . htmlspecialchars($userName)."!";
+    } else { echo "Failed to Login."; }
+}
+$conn -> close();
+?>
